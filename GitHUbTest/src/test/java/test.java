3d7c426067bd12ed623948ca96ded29d4daddece
Sushma <Sushma@DESKTOP-EQIB10L>
2023-08-24 08:21:09 +0530

    Merge branch 'master' of https://github.com/sushramu/githubTest.git

--- GitHUbTest/src/test/java/test.java
+++ GitHUbTest/src/test/java/test.java
@@ -6,9 +6,7 @@ public class test {
 	{
 		int AddedbyFirst;
 		System.out.println("Test");
-
 		System.out.println("From Second engineer");
-
 		System.out.print("From first system engineer");
 
 	}
