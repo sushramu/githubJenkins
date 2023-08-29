c3ad1b2c6bf9015f2d1f0bb7dd1d0cafdb102502
sushramu <sushramu@gmail.com>
2023-08-29 22:29:37 +0530

    push changes

---
+++ GitHUbTest/src/test/java/HelloTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class HelloTest {
+	@Test
+	public void helloTest()
+	{
+		int addedbyFirst;
+		System.out.println("Test");
+		System.out.println("From Second engineer");
+		System.out.print("From first system engineer");
+
+	}
+//adding comment
+}
